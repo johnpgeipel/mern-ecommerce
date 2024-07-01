4d78fd0208d6fd6251b76a5d367c8e33237d6645
johnpgeipel <johnpgeipel@gmail.com>
2024-07-01 15:21:10 -0500

    rm whitespce

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,5 @@
 const User = require('../models/user');
 
-// exports.signup = (req, res) => {
-//     try {
-//         const user = new User(req.body);
-//         console.log("req.body", req.body);
-//         user = user.save().then(
-//             res.status(200).json({
-//                 message: "Created",
-//                 user,
-//             })
-//         )
-//     } catch (err) {
-//         res.status(500).json({
-//           message:
-//             "error data was not created",
-//         });
-//     }
-// };
-
 exports.signup = async (req, res) => {
     try {
         const user = new User(req.body);
@@ -37,12 +19,3 @@ exports.signup = async (req, res) => {
         });
     }
 };
-
-// exports.signup = async (req, res) => {
-//     console.log("req.body", req.body)
-
-//     const user = new User(req.body);
-//     let savedUser = await user.save();
-//     console.log("savedUser", savedUser);
-//     console.log("user", user);
-// }
