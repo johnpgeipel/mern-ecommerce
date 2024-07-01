78cbf31aa35b9d51f836860b06f4bc14468be35d
johnpgeipel <johnpgeipel@gmail.com>
2024-07-01 18:09:55 -0500

    user controller cleanup

--- controllers/user.js
+++ controllers/user.js
@@ -3,13 +3,13 @@ const User = require('../models/user');
 exports.signup = async (req, res) => {
     try {
         let user = new User(req.body);
-        
+
         user = await user.save();
-        res.status(200).json({
+        res.json({
             user
         });
     } catch (err) {
-        res.status(500).json({
+        res.json({
             err
         });
     }
