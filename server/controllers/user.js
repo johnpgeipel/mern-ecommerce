b44b4abcc5b161120491254ed5ea0f4e8b7b1522
johnpgeipel <johnpgeipel@gmail.com>
2024-07-30 17:33:42 -0500

    rewriting callbacks for findOne() method depriciation - WIP

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -23,30 +23,35 @@ exports.signup = async (req, res) => {
 
 exports.signin = async (req, res) => {
     // find the user based on email
-    const { email, password } = req.body;
-    User.findOne({email}, (err, user) => {
-        if (err || !user) {
-            return res.status(400).json({
-                error: 'User with that email does not exist. Please sign up'
-            })
-        }
-        // if user found make sure email and password match
-        // create auth method in user model
-        if (!user.authenticate(password)) {
-            return res.status(401).json({
-                error: 'Email and password do not match'
-            })
-        }
+    try {
+        const { email, password } = req.body;
+        let user = await User.findOne({email})
+        res.json({
+            user
+        });
+        
+        
+    } catch (err) {
+        res.json({
+            err: errorHandler(err)
+        });
+    }
 
-        // generate signed token with user id and secret
-        const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET);
-        // persist the token as 't' in cookie with expire date
-        res.cookie('t', token, {expire: new Date() + 9999});
-        // return res w user and token to frontend client
-        const {_id, name, email, role } = user;
-        return res.json({token, user: {_id, email, name, role}})
-    })
+        // const { email, password } = req.body;
+        // User.findOne({ email })
+        //     .then((user) => {
+        //         console.log(user)
+        //         res.json({
+        //             user
+        //         })
+        //     })
+        // .catch((err) => {
+        //     res.json({
+        //         err: errorHandler(err)
+        //     });
+        // })
 
+   
 
   
     
