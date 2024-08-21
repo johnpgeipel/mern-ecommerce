5933c20a6f1a028c48be560ed7cb6fee3b07b1b3
johnpgeipel <johnpgeipel@gmail.com>
2024-08-21 16:53:36 -0500

    Lesson 35 - product controller wip - debugging db callback Mongodb issue on save()

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
-exports.create = async (req, res) => {
+exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
 
     form.keepExtensions = true
 
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, async (err, fields, files) => {
         if (err) {
             return res.status(400).json({
                 error: 'Image could not be uploaded'
@@ -25,20 +25,30 @@ exports.create = async (req, res) => {
               });
             }
             product.photo.data = fs.readFileSync(files.photo.filepath); // change path to filepath
-            product.photo.contentType = files.photo.mimetype; // change typt to mimetype
+            product.photo.contentType = files.photo.mimetype; // change type to mimetype
         }
 
-        product.save((err, result) => {
-            if (err) {
-                return res.status(400).json({
-                    error: errorHandler(err)
-                });
-            }
-            res.json({
-                result
-            })
+        // product.save((err, result) => {
+        //     if (err) {
+        //         return res.status(400).json({
+        //             error: errorHandler(err)
+        //         });
+        //     }
+        //     res.json({
+        //         result
+        //     })
             
-        })
+        // });
+        try {
+            const data = await product.save();
+            res.json({
+                data
+            });
+        } catch (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        };
     })
 }
 
