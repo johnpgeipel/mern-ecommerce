636b1fe186d8fbb162b112f81d9042c8d11d9cb7
johnpgeipel <johnpgeipel@gmail.com>
2024-09-06 15:27:58 -0500

    Lesson 40 - GET categoryById controller & read middleware

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,6 +1,18 @@
 const Category = require('../models/category');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.categoryById = async (req, res, next, id) => {
+    try {
+        const category = await Category.findById(id);
+        req.category = category;
+        next();
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler(err) || 'Catergory does not exist'
+        });
+    }
+};
+
 exports.create = async (req, res) => {
     try {
         const category = new Category(req.body);
@@ -14,4 +26,7 @@ exports.create = async (req, res) => {
             error: errorHandler(err)
         });
     };
+}
+exports.read = (req, res) => {
+    return res.json(req.category);
 }
--- server/routes/category.js
+++ server/routes/category.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category');
+const { create, categoryById, read } = require('../controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
@@ -12,7 +12,12 @@ router.post(
     isAdmin,
     create
 );
+router.get(
+    '/category/:categoryId',
+    read
+)
 
-router.param('userId', userById)
+router.param('categoryId', categoryById);
+router.param('userId', userById);
 
 module.exports = router;
